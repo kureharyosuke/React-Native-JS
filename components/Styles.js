63c08a512eaa1823ded9e2b7336159874397bf36
kureharyosuke <ohhayo.com@gmail.com>
2021-12-05 22:29:59 +0900

    add: styles

--- components/Styles.js
+++ components/Styles.js
@@ -28,3 +28,34 @@ export default function Styles() {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "tomato",
+  },
+  city: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 68,
+    fontWeight: "500",
+  },
+  weather: {
+    flex: 3,
+  },
+  day: {
+    flex: 1,
+    alignItems: "center",
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 178,
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 60,
+  },
+});
