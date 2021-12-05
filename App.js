b7e622c1f7ac45e9c98e93f4935d92430a9337aa
kureharyosuke <ohhayo.com@gmail.com>
2021-12-05 20:14:11 +0900

    View: div span, Text h1,h2, p

--- App.js
+++ App.js
@@ -4,18 +4,35 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>React Native</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+        border: "1px solid black",
+      }}
+    >
+      <Text style={styles.text}>React Native</Text>
+      <StatusBar style="light" />
+      {/* clock, wifi, battery by status bar  */}
     </View>
   );
 }
 
+// StyleSheet.create({}) create a style object
+// Stylesheet is auto
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "red",
     alignItems: "center",
     justifyContent: "center",
+    border: "1px solid black",
+  },
+  text: {
+    fontSize: 30,
+    color: "red",
   },
 });
